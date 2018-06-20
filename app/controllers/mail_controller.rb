63944884392a2b70cde6dfc70a3df908a7a417a4
TheLocust3 <jake.kinsella@gmail.com>
2018-06-20 09:09:50 -0400

    Add reset password endpoint

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,6 +1,6 @@
 class MailController < ApplicationController
     include Authenticatable
-    before_action :authenticate
+    before_action :authenticate, except: [ :mail_reset_password ]
 
     def mail_as_company
         parameters = mail_as_company_params(params)
@@ -8,6 +8,19 @@ class MailController < ApplicationController
         Mailer.send_as_company(parameters[:email], parameters[:subject], parameters[:body]).deliver_later
 
         render :json => { 'status': 'ok' }
+    end
+
+    def mail_reset_password
+        body = "
+            <p>Someone has requested a link to change your password. You can do this through the link below.</p>
+    
+            <p><a href='#{Rails.application.secrets.auth_api}/auth/password/edit?config=default&redirect_url=#{CGI.escape(params[:redirect_url])}&reset_password_token=#{params[:token]}'>Change my password</a></p>
+            
+            <p>If you didn&#39;t request this, please ignore this email.</p>
+            <p>Your password won&#39;t change until you access the link above and create a new one.</p>
+        "
+  
+        Mailer.send_as_company(params[:email], 'Reset Password Request', body).deliver_later
     end
 
     def mail_as_campaign
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post 'company' => 'mail#mail_as_company'
+  post 'reset_password' => 'mail#mail_reset_password'
   post 'campaign' => 'mail#mail_as_campaign'
   post 'campaign_many' => 'mail#mail_many_as_campaign'
 end
